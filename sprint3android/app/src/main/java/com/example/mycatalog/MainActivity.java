f290183c2784225b5fdd241ef9da0b10fa3853ca
Yago Pazos <ypazosl24@fpcoruna.afundacion.org>
2025-11-10 10:50:26 +0100

    Tarea #2,completa

--- sprint3android/app/src/main/java/com/example/mycatalog/MainActivity.java
+++ sprint3android/app/src/main/java/com/example/mycatalog/MainActivity.java
@@ -7,6 +7,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import android.widget.Button;
+import android.content.Intent;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,5 +17,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        Button navigateButton = findViewById(R.id.btn_navegar);
+
+        navigateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(MainActivity.this, DetailActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
